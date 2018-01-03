db9fb1f9237cddb983ca9a72a8e3a6442cda305b
Lisa Herzberg <lisaherzberg@Lisas-MacBook-Pro.local>
2018-01-03 13:53:48 -0700

    create page complete need to add function

--- src/components/Create/CreateGoal.js
+++ src/components/Create/CreateGoal.js
@@ -18,13 +18,17 @@ class CreateGoal extends Component {
                 <Header/>
                 <div className="rapperLarge">
                     <div className="titleText">Create your Goal</div>
-                    <p>Create a goal. You are able to make 10 goals. Once you create a goal <br/>
+                    <p>Create a goal. You are able to create up to 10 goals. Once you create a goal <br/>
                     you can add tasks to it.</p>
-                    <form>
-                        <input className="inputs" type="text" placeholder="Goal Title (20 Char limit)"/>
+                    <form className="formyForm">
+                        <input className="inputs" type="text" placeholder="Goal Title (20 character limit)"/>
                         <input className="inputs" type="text" placeholder="Goal Description"/>
                         <input className="datess" type="text" placeholder="Start Date"/>
                         <input className="datess" type="text" placeholder="End Date"/>
+                        <div>
+                            <button style={buttonLarger} className="buttonGoal">Reset</button>
+                            <button style={buttonLarger} className="buttonGoal">Create Goal</button>
+                        </div>
                     </form>
                 </div>
                 <Footer/>
@@ -34,5 +38,8 @@ class CreateGoal extends Component {
 }
 
 
-
+const buttonLarger = {
+    fontSize: '15pt',
+    width: '150px'
+}
 export default CreateGoal
