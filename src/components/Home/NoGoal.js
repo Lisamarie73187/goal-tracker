4b7f30f98e0d56131dccaf63c6b2e158fd2693df
Lisa Herzberg <lisamarieherzberg@gmail.com>
2018-01-05 09:26:29 -0700

    testing

---
+++ src/components/Home/NoGoal.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+
+class NoGoal extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>test</h1>
+            </div>
+        )
+    }
+}
+
+
+
+export default NoGoal
